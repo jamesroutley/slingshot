f61e60cf43df06b5238b3a9a439ea5932194bfdd
James Routley <jroutley@gmail.com>
2017-06-10 16:23:25 -0400

    Add finite fuel

--- src/spaceship.js
+++ src/spaceship.js
@@ -10,6 +10,7 @@ class SpaceShip {
     this.a = new p5.Vector(0, 0);
     this.booster_a = new p5.Vector(0, 0);
     this.rotation = 0;
+    this.fuel = 1800;
     // TODO: move these to constants.js
     this.width = 6;
     this.length = 10;
@@ -21,11 +22,17 @@ class SpaceShip {
     // TODO: reimplement this using p5.Vector.fromAngle()
     // and scalar multiplying it by the force.
     // Force vector initially points up (0, -1)
+    if (this.fuel <= 0) {
+      this.stopBoost();
+      console.log('no fuel');
+      return;
+    }
     const force = new p5.Vector(
       0, -this.rocketForcePerS / constants.frameRate);
     // Rotate it to match direction of spaceship
     force.rotate(this.rotation);
     this.booster_a = physics.acceleration(force, this.mass);
+    this.fuel -= 1;
   }
 
   stopBoost() {
@@ -72,7 +79,8 @@ class SpaceShip {
     p.translate(this.pos.x, this.pos.y);
     p.rotate(this.rotation);
     p.rect(-3, -5, this.width, this.length, 2);
-    if (p.keyIsPressed && p.key === ' ') {
+    // TODO: move this logic into boost() ?
+    if (p.keyIsPressed && p.key === ' ' && this.fuel > 0) {
       p.fill('#ff8a65');
       p.rect(-3, 4, 6, 3);
     }
